cac0e23f5378df73b57a30bdb5ce652330021eec
ijin <2zun@naver.com>
2021-12-22 13:33:30 +0900

    초기 셋팅 groupId: jpabook artifactId: jpashop 사용 기능: web, thymeleaf, jpa, h2, lombok

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
